70ea047c54740ba41f41b28c481f6a7a8cdb00b4
geoteknik <89979082+geoteknik@users.noreply.github.com>
2024-12-02 12:30:56 +0900

    Revisi M --> CSL

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Harya1\AppData\Local\Temp\tmphqzyh2mq.js
+// include: C:\Users\Harya1\AppData\Local\Temp\tmpe2xd93mi.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -224,21 +224,21 @@ Module['FS_createPath']("/", "fonts", true, true);
 
   })();
 
-// end include: C:\Users\Harya1\AppData\Local\Temp\tmphqzyh2mq.js
-// include: C:\Users\Harya1\AppData\Local\Temp\tmpof7qsgwv.js
+// end include: C:\Users\Harya1\AppData\Local\Temp\tmpe2xd93mi.js
+// include: C:\Users\Harya1\AppData\Local\Temp\tmpbvnv_9vs.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Harya1\AppData\Local\Temp\tmpof7qsgwv.js
-// include: C:\Users\Harya1\AppData\Local\Temp\tmpmb1dh1h7.js
+  // end include: C:\Users\Harya1\AppData\Local\Temp\tmpbvnv_9vs.js
+// include: C:\Users\Harya1\AppData\Local\Temp\tmpv5bsd8ar.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Harya1\AppData\Local\Temp\tmpmb1dh1h7.js
+  // end include: C:\Users\Harya1\AppData\Local\Temp\tmpv5bsd8ar.js
 
 
 // Sometimes an existing Module object exists with properties
